bf6a0bf09275944a72f4e0990f785ed04d157274
mira-yoon <amorfati823@gmail.com>
2023-01-30 03:10:43 +0900

    기본세팅 및 route구현

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BlogDetail from './pages/BlogDetail/BlogDetail'
+import Home from './pages/Home/Home'
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route 
+					path="/" 
+					element={<Home/>}
+				/>
+				<Route 
+					path="/blog/:id"
+					element={<BlogDetail/>}
+				/>
+			</Routes>
+		</BrowserRouter>
+	)
+}
+
+export default App;
