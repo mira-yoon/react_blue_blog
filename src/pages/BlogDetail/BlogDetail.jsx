e61c768368f7a2b9f9e64c5072c1c06d8f40ea1e
mira-yoon <amorfati823@gmail.com>
2023-01-30 22:45:35 +0900

    json 서버 실행 및 axios로 blogDetail페이지에 데이터 불러오기

--- src/pages/BlogDetail/BlogDetail.jsx
+++ src/pages/BlogDetail/BlogDetail.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import axios from 'axios'
+
 export default function BlogDetail() {
+  const location = useLocation();
+  const postNumber = location.pathname.split('/')[2];
+
+  const [post, setPost] = useState(undefined)
+  useEffect(()=>{
+    axios
+    .get(`http://localhost:3001/posts/${postNumber}`)
+    .then((json)=>{
+      setPost(json.data)  
+    })
+    .catch(()=>{
+      setPost(null)
+    })
+  },[])
+
   return (
-    <div>BlogDetail</div>
+    <>
+      {post === undefined ? <div>로딩 중</div> : post === null ? <div>404</div> : <div>{post.title}</div>}
+    </>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import "./home.css"
+
 export default function Home() {
+  const [posts, setPosts] = useState([])
+  useEffect(()=>{
+    axios
+    .get("http://localhost:3001/posts")
+    .then((json)=>{
+      setPosts(json.data)  
+    })
+  },[])
+  console.log(posts)
   return (
-    <div>Home</div>
+    <div>
+      Home
+      {posts.map(post=><div key={post.id}>{post.title}</div>)}
+    </div>
   )
 }
