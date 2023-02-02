c100d3813c7119f33d46d1b4ad3a851ae64a770c
mira-yoon <amorfati823@gmail.com>
2023-02-02 09:04:43 +0900

    route path 절대경로를 home 컴포넌트에만 적용해서 수정

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 					element={<Home/>}
 				/>
 				<Route 
-					path={process.env.PUBLIC_URL + '/blog/:id'}
+					path='/blog/:id'
 					element={<BlogDetail/>}
 				/>
 			</Routes>
